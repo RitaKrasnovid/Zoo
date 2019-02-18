118a9bf29f2124f34185ea1da0ef135d53f71774
RitaKrasnovid <mkrasnovid@lineate.com>
2019-02-18 00:27:24 -0800

    filtering-list-by-name change detection strategy

--- frontend/src/app/animals-list/animals-list.component.ts
+++ frontend/src/app/animals-list/animals-list.component.ts
@@ -1,36 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { AnimalApiService } from '../services/animal.service';
 import { Animal } from '../models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-animals-list',
   templateUrl: './animals-list.component.html',
   styleUrls: ['./animals-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AnimalsListComponent implements OnInit {
-  public animals: Animal[] = [];
+  public animals$: Observable<Animal[]>;
+  countResult: number;
 
   constructor(private apiService: AnimalApiService) { }
 
   ngOnInit() {
-    this.apiService.getAnimals().subscribe(
-      (data: Animal[]) => {
-        this.animals = data;
-      },
-      err => console.log(err),
-      () => console.log('Animals list component completed')
-    );
+    this.animals$ = this.apiService.getAnimals();
   }
 
   filterByName(filterValue) {
     if (filterValue.search_type.type === 'contains') {
-      this.apiService.filterByNameContainsValue(filterValue.search_value).subscribe(
-        (results: Animal[]) => {
-          this.animals = results;
-        },
-        err => console.log(err)
-      );
-    }
+      this.animals$ = this.apiService.filterByNameContainsValue(filterValue.search_value);
+     }
   }
 
 }
